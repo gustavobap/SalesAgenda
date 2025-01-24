b5c36708f5f8314498630ccf7de8e7b195b8ae44
Gustavo Peixoto <gustavo.peixoto.code@gmail.com>
2025-01-25 00:17:51 +0100

    Use host user UID also for docker user for development convenience

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,13 @@ set -euxo pipefail
 docker build -t sales-agenda-db database
 docker start sales-agenda-db || docker run --name sales-agenda-db -p 5432:5432 -d sales-agenda-db
 
-docker build -t sales-agenda-api api
+docker build -t sales-agenda-api api \
+--build-arg UID="$(id -u)" \
+--build-arg UNAME="$(whoami)" \
+--build-arg UPASSWORD="$(whoami)"
+
 docker start sales-agenda-api || docker run --name sales-agenda-api -p 3000:3000 \
--v ./api/sales-agenda-api:/home/gustavo/sales-agenda-api:rw \
+-v ./api/sales-agenda-api:/home/$(whoami)/sales-agenda-api \
 -it -d sales-agenda-api
 
 docker exec -it sales-agenda-api /bin/bash
