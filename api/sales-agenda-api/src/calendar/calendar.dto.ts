e875e48f4d18b3fd1ee58c57a7961983b9fbed6e
Gustavo Peixoto <gustavo.peixoto.code@gmail.com>
2025-01-29 16:01:32 +0100

    add validations to the API requests

--- api/sales-agenda-api/src/calendar/calendar.dto.ts
+++ api/sales-agenda-api/src/calendar/calendar.dto.ts
@@ -1,8 +1,31 @@
-export interface QueryAvailableSlotsFiltersDTO {
-  date: Date,
-  products?: Array<"Heatpumps" | "SolarPanels">,
-  language?: "German" | "English",
-  rating?: "Gold" | "Silver" | "Bronze"
+import { IsIn, IsISO8601, IsOptional } from "class-validator"
+
+
+const AVAILABLE_LANGUAGES = ["German", "English"] as const
+export type AvailableLanguage = typeof AVAILABLE_LANGUAGES[number]
+
+const AVAILABLE_RATINGS = ["Gold", "Silver", "Bronze"] as const
+export type AvailableRating = typeof AVAILABLE_RATINGS[number]
+
+const AVAILABLE_PRODUCTS = ["Heatpumps", "SolarPanels"] as const
+export type AvailableProduct = typeof AVAILABLE_RATINGS[number]
+
+export class QueryAvailableSlotsFiltersDTO {
+
+  @IsISO8601({ strict: true })
+  date: string
+
+  @IsOptional()
+  @IsIn(AVAILABLE_PRODUCTS, { each: true })
+  products?: Array<AvailableProduct>
+
+  @IsOptional()
+  @IsIn(AVAILABLE_LANGUAGES)
+  language?: AvailableLanguage
+
+  @IsOptional()
+  @IsIn(AVAILABLE_RATINGS)
+  rating?: AvailableRating
 }
 
 export type QueryAvailableSlotsResultDTO = Array<{
