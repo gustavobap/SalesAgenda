f5573ae099f78f8e9fe4909eb24d1e67cb4077bd
Gustavo Peixoto <gustavo.peixoto.code@gmail.com>
2025-01-29 16:37:50 +0100

    change docker file to run in production

--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export DOCKER_UID="$(id -u)"
-export DOCKER_UNAME="$(whoami)"
-export DOCKER_UPASSWORD="$(whoami)"
-docker-compose -f "./docker-compose.yml" run --rm --service-ports $1
